0f2079eb459e4c5e7195b8f90de4e875e3e2ac50
Pascal Poizat <pascal.poizat@gmail.com>
2026-03-27 13:36:59 +0100

    feat: commit message with information = add method mul + tests

--- lib/src/main/java/org/example/Library.java
+++ lib/src/main/java/org/example/Library.java
@@ -11,4 +11,9 @@ public class Library {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
 }
---
+++ lib/src/test/java/org/example/LibraryTestSuite1.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestSuite1 {
+
+    private Library lib;
+
+    @BeforeEach
+    void setup() {
+        lib = new Library();
+    }
+
+    // a series of tests for a single method
+    // with no relation to the method name in the test names
+    // and included in an additional test file
+    // also, method call directly in assert
+    @Test
+    void zero_yields_zero() {
+        assertEquals(0, lib.mul(0, 0));
+        assertEquals(0, lib.mul(0, 1));
+        assertEquals(0, lib.mul(1, 0));
+    }
+
+    @Test
+    void one_is_neutral() {
+        assertEquals(1, lib.mul(1,1));
+        assertEquals(2, lib.mul(2,1));
+        assertEquals(2, lib.mul(1,2));
+    }
+
+    @Test
+    void other_cases() {
+    assertEquals(12, lib.mul(3, 4));
+    assertEquals(12, lib.mul(4, 3));
+    }
+
+}
