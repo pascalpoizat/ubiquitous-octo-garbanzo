a05420d30466c4150dd36afa38fd6c14c2f63fd1
Pascal Poizat <pascal.poizat@gmail.com>
2026-03-27 13:54:08 +0100

    refactor: some renaming

--- lib/src/test/java/org/example/LibraryTestSuite1.java
+++ lib/src/test/java/org/example/LibraryTestSuite1.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestSuite1 {
+class LibraryTestSuite1 {
 
     private Library lib;
 
--- lib/src/test/java/org/example/LibraryTestSuite2.java
+++ lib/src/test/java/org/example/LibraryTestSuite2.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestSuite2 {
+class LibraryTestSuite2 {
   
     private Library lib;
 
